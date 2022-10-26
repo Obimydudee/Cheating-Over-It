a52f3d19da3ec6547ee5d1bf54a90ecedc2e9e84
Obi1 <benjaminhansson3.com@gmail.com>
2022-10-26 03:23:16 +0200

    Add files via upload

---
+++ CheatingOverIt/CheatingOverIt/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+namespace CheatingOverIt
+{
+    public class Class1 : MelonMod
+    {
+        public static Color pissypiss;
+        public override void OnApplicationStart()
+        {
+            base.OnApplicationStart();
+            //MelonLogger.Log("Starting...");
+        }
+        public override void OnGUI()
+        {
+            base.OnGUI();
+            if (!Input.GetKey(KeyCode.Q)) return; //(!Input.GetKey(KeyCode.Insert))
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            GUI.contentColor = Color.white;
+            GUI.backgroundColor = pissypiss; //Color.magenta;
+            GUI.Box(new Rect(10, 10, 380, 300), "CheatingOverIt Menu");
+            if (GUI.Button(new Rect(20, 40, 160, 30), "TP UP LOL"))
+            {
+                GameObject bruhh = GameObject.Find("/Player");
+                Vector3 Poz = new Vector3(64, 433.68f);//bruhh.transform.position.magnitude;
+                bruhh.transform.position = Poz;
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            pissypiss = new((float)Math.Sin(2 * Time.time * 0.5f + 0.5f), (float)Math.Sin((2 * Time.time) + Math.PI * 2.0 / 3.0) * 0.5f + 0.5f, (float)Math.Sin((2 * Time.time) + Math.PI * 4.0 / 3.0) * 0.5f + 0.5f);
+
+        }
+    }
+}
---
+++ CheatingOverIt/CheatingOverIt/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: MelonLoader.MelonInfo(typeof(CheatingOverIt.Class1), "CheatingOverIt", "1", "NulledCunt", "why do i even bother at this point lol")]
+[assembly: MelonLoader.MelonGame(null, null)]
+[assembly: MelonColor(ConsoleColor.Magenta)]
+[assembly: AssemblyTitle("CheatingOverIt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CheatingOverIt")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9af1b1c4-342c-4e17-87ca-04f8e2e6412d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
